69fb6e2c3ac0bbd19bf479d26cee7b4e44b699eb
jeongmun <lot9616@gmail.com>
2022-04-08 14:29:30 +0900

    Product 모델 생성

--- common/models.py
+++ common/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+	title = models.CharField(max_length=30)
+	description = TextField()
+	target_amount = IntegerField()
+	end_date = DateField()
+	amount_per_time = IntegerField()
