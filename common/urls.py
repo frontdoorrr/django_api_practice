6171241b40237b9220321d9640a64c3a93100892
jeongmun <lot9616@gmail.com>
2022-04-11 16:33:06 +0900

    detail view 추가

--- common/urls.py
+++ common/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
 	path('', views.ProductList.as_view()),
+	path('<int:pk>', views.ProductDetail.as_view())
 ]
--- common/views.py
+++ common/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Product
 
 # Create your views here.
@@ -8,3 +8,6 @@ class ProductList(ListView):
 	model = Product
 	template_name = 'common/index.html'
 
+class ProductDetail(DetailView):
+	model = Product
+	template_name = 'common/detail.html'
